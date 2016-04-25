609f323166e54c8b148cb983288b24461894a722
Hagen Hübel <hhuebel@itinance.com>
2016-04-25 17:07:45 +0200

    initial

---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppBundle extends Bundle
+{
+    /**
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'SyliusWebBundle';
+    }
+
+}
